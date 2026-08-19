c912802e7e7f78ff8136c9b8e29228b1b4c55237
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-19 19:47:38 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -14,17 +14,90 @@ if (command !== "tokenize") {
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.error("Logs from your program will appear here!");
-
 const filename = args[1];
 
-// TODO: Uncomment the code below to pass the first stage
+const fileContent = fs.readFileSync(filename, "utf8");
 
- const fileContent = fs.readFileSync(filename, "utf8");
+// Token type identifiers
+const TokenType = {
+  LEFT_PAREN: "LEFT_PAREN",
+  RIGHT_PAREN: "RIGHT_PAREN",
+  EOF: "EOF",
+};
 
- if (fileContent.length !== 0) {
-   throw new Error("Scanner not implemented");
- } else {
-   console.log("EOF  null");
- }
+class Token {
+  constructor(type, lexeme, literal, line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  toString() {
+    return `${this.type} ${this.lexeme} ${this.literal}`;
+  }
+}
+
+class Scanner {
+  constructor(source) {
+    this.source = source;
+    this.tokens = [];
+    this.start = 0;
+    this.current = 0;
+    this.line = 1;
+  }
+
+  scanTokens() {
+    while (!this.isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push(new Token(TokenType.EOF, "", null, this.line));
+    return this.tokens;
+  }
+
+  isAtEnd() {
+    return this.current >= this.source.length;
+  }
+
+  scanToken() {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case " ":
+      case "\r":
+      case "\t":
+        // Ignore whitespace.
+        break;
+      case "\n":
+        this.line++;
+        break;
+      default:
+        throw new Error(`Unexpected character: ${c}`);
+    }
+  }
+
+  advance() {
+    this.current++;
+    return this.source.charAt(this.current - 1);
+  }
+
+  addToken(type, literal = null) {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal, this.line));
+  }
+}
+
+const scanner = new Scanner(fileContent);
+const tokens = scanner.scanTokens();
+
+for (const token of tokens) {
+  console.log(token.toString());
+}
